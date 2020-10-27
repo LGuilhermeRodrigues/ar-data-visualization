6f05e8bc9efc3b75c927d3a69d3df4cfca7f6d7e
Luis Guilherme Silva Rodrigues <guilherme.rodrigues@unesp.br>
2020-10-26 22:50:50 -0300

    all 4 markers is added to the scene

---
+++ Assets/Scripts/StateChangeChart.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateChangeChart : MonoBehaviour
+{
+
+    public int dataQuery;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int[] data = {1, 2, 3, 5, 5, 6, 3, 1};
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
