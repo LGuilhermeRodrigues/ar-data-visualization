31363b3e8eb9e2b1fea548aaf37bb4d087964ddf
Luis Guilherme Silva Rodrigues <guilherme.rodrigues@unesp.br>
2020-11-14 21:22:15 -0300

    creating bars according to dims

--- Assets/Vuforia/Scripts/ChartRendererScript.cs
+++ Assets/Vuforia/Scripts/ChartRendererScript.cs
@@ -7,6 +7,11 @@ namespace RenderSpace
 {
     public class ChartRendererScript : MonoBehaviour
     {
+        static float luisScale = 1f;
+        private Vector3 commonScale = new Vector3(luisScale*0.3f, luisScale* 0.3f, luisScale*0.3f);
+        private float rangeX = luisScale*2f;
+        private float rangeY = luisScale * 1f;
+
         // Start is called before the first frame update
         void Start()
         {
@@ -22,22 +27,17 @@ namespace RenderSpace
         public void renderBars(GameObject parent, List<string> list1, List<string> list2)
         {
             List<string> uniqueValues = list1.Distinct().ToList();
-            for (int i = 0; i < 0; i++)//uniqueValues.Count
+            float xStepSize = rangeX / uniqueValues.Count;
+            float xStart = 0-rangeX/2;
+            for (int i = 0; i < uniqueValues.Count; i++)
             {
-
-                GameObject gameObject = GameObject.CreatePrimitive(PrimitiveType.Capsule);
-                gameObject.transform.parent = parent.transform;
-                gameObject.transform.position = parent.transform.position;
-                gameObject.transform.rotation = this.gameObject.transform.rotation;
-                //gameObject.transform.position = new Vector3(gameObject.transform.position.x + i * 0.02f, gameObject.transform.position.y, gameObject.transform.position.z + i * 0.02f);
-                gameObject.transform.localScale = this.gameObject.transform.localScale;
+                GameObject gObject = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+                gObject.transform.parent = parent.transform;
+                gObject.transform.rotation = parent.transform.rotation;
+                gObject.transform.localScale = commonScale;
+                gObject.transform.localPosition = new Vector3(xStart+i*xStepSize, commonScale.y, 0.0f);
             }
-
-            GameObject gObject = GameObject.CreatePrimitive(PrimitiveType.Capsule);
-            gObject.transform.parent = parent.transform;
-            gObject.transform.position = parent.transform.position;
-            gObject.transform.rotation = parent.transform.rotation;
-            gObject.transform.localScale = new Vector3(1,1,1);
+            
         }
     }
 }
